f47c54f476b8d6361422f551c54a9dd1bd9cfd13
Juan Martinez Guevara <JuanJMtzGvra@Juans-Macbook.attlocal.net>
2021-02-19 23:26:42 -0600

    Improve Playlist component

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Destinations(props) {
-    return (
-        <div>
-            <h1>Step 1: Destinations</h1>
-            <div className='buttons-row'>
-            <Link to='/'><button>Previous: Home</button></Link>
-            <Link to='/attractions'><button>Next: Attractions</button></Link>
-            </div>
-        </div>
-    );
+
+
+  return (
+    <div>
+      <h1>Step 1: Destinations</h1>
+      <div className="buttons-row">
+        <Link to="/">
+          <button>Previous: Home</button>
+        </Link>
+        <Link to="/attractions">
+          <button>Next: Attractions</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Destinations;
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,99 +1,134 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
-import token from './Token'
+import token from "./Token";
 
 function Playlist(props) {
   let [categories, setCategories] = useState([""]);
-  let [category, setCategory] = useState('');
+  let [category, setCategory] = useState("");
   let [playlists, setPlaylists] = useState([]);
   let [tracks, setTracks] = useState([]);
+  let [allTracks, setAllTracks] = useState([]);
 
   useEffect(() => {
     getSpotifyData();
-   }, []);
-   async function getSpotifyData() {
-     fetch(`https://api.spotify.com/v1/browse/categories?limit=50`, {
-       headers: {
-         Accept: 'application/json',
-         Authorization: `Bearer ${await token()}`,
-         'Content-Type': 'application/json'
-       }
-     })
-       .then((res) => res.json())
-       .then((data) => {
-         console.log(data);
-         setCategories(data.categories.items);
-       });
-   }
+  }, []);
 
-   async function getCategoryData(category_id) {
-    setCategory(category_id)
-    fetch(`https://api.spotify.com/v1/browse/categories/${category_id}/playlists`, {
+  async function getSpotifyData() {
+    fetch(`https://api.spotify.com/v1/browse/categories?limit=50`, {
       headers: {
-        Accept: 'application/json',
+        Accept: "application/json",
         Authorization: `Bearer ${await token()}`,
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.playlists);
-        setPlaylists(data.playlists.items);
+        console.log("getSpotifyData()", data);
+        setCategories(data.categories.items);
       });
   }
-//https://api.spotify.com/v1/browse/categories/37i9dQZF1DWUa8ZRTfalHk/tracks
-  async function getTrackData(playlist_id) {
-    fetch(`	https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, {
-      headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${await token()}`,
-        'Content-Type': 'application/json'
+
+  async function getCategoryData(category_id) {
+    setCategory(category_id);
+    fetch(
+      `https://api.spotify.com/v1/browse/categories/${category_id}/playlists`,
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${await token()}`,
+          "Content-Type": "application/json",
+        },
       }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("playlists", data.playlists);
+        setPlaylists(data.playlists.items);
+        data.playlists.items.map(async (eachPlaylist) => {
+          fetch(
+            `https://api.spotify.com/v1/playlists/${eachPlaylist.id}/tracks`,
+            {
+              headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${await token()}`,
+                "Content-Type": "application/json",
+              },
+            }
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              console.log("tracks", data.items);
+              setAllTracks(...tracks, data.items);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      });
+  }
+
+  async function getTrackData(playlist_id) {
+    fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${await token()}`,
+        "Content-Type": "application/json",
+      },
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log('pumpkin',data, data.items);
+        console.log("tracks", data.items);
         setTracks(data.items);
       });
   }
 
-   const ShowCategories = () => {
-       return categories.map(eachCat => {
-           return (
-               <li onClick={() => getCategoryData(eachCat.id)}>
-                   {eachCat.name}
-               </li>
-           )
-       })
-   }
+//   const showCategories = () => {
+//     return categories.map((eachCat) => {
+//       return (
+//         <li onClick={() => getCategoryData(eachCat.id)}>{eachCat.name}</li>
+//       );
+//     });
+//   };
 
-   const ShowPlaylist = () => {
-       return playlists.map(eachPlaylist => {
-           return (
-               <li onClick={() => getTrackData(eachPlaylist.id)}>
-                   {eachPlaylist.name}
-               </li>
-           )
-       })
-   }
+//   const showPlaylist = () => {
+//     return playlists.map((eachPlaylist) => {
+//       return (
+//         <li onClick={() => getTrackData(eachPlaylist.id)}>
+//           {eachPlaylist.name}
+//         </li>
+//       );
+//     });
+//   };
 
-   const ShowTracks = () => {
-    console.log(tracks)
-    return tracks.map(eachTrack => {
-        if(eachTrack.track.preview_url){
-        return (
-            <li>
-                {eachTrack.track.preview_url}
-                    <audio controls>
-                    <source src={eachTrack.track.preview_url} type="audio/mpeg"/>
-                    </audio>
-            </li>
-        )
-        }
-    })
-}
+//   const showTracks = () => {
+//     return tracks.map((eachTrack) => {
+//       if (eachTrack.track.preview_url) {
+//         return (
+//           <li>
+//             {eachTrack.track.preview_url}
+//             <audio controls>
+//               <source src={eachTrack.track.preview_url} type="audio/mpeg" />
+//             </audio>
+//           </li>
+//         );
+//       }
+//     });
+//   };
+
+  const getChillPlaylist = (e) => {
+    getCategoryData("chill");
+    tracks.map(each => {
+      return (
+        <li>
+          {each.track.name}
+          <audio controls>
+            <source src={each.track.preview_url} type="audio/mpeg" />
+          </audio>
+        </li>
+      );
+    });
+  };
 
   return (
     <div>
@@ -105,46 +140,45 @@ function Playlist(props) {
           </h3>
 
           <div>
-            <input type="radio" value="A" />
-            <label>Party</label>
+            <input type="radio" value="pop" />
+            <label>Adventurous</label>
           </div>
 
-          <div>
-            <input type="radio" value="B" />
-            <label>Relaxing</label>
+          <div onChange={getChillPlaylist}>
+            <input type="radio" value="chill" />
+            <label>Chill</label>
           </div>
 
           <div>
             <input type="radio" value="C" />
-            <label>Wholesome</label>
+            <label>Party</label>
+          </div>
+
+          <div>
+            <input type="radio" value="C" />
+            <label>Romantic</label>
           </div>
 
           <div>
             <input type="radio" value="D" />
-            <label>Introspective</label>
+            <label>Wholesome</label>
           </div>
         </ul>
       </form>
-      <h1>{category}</h1>
-      tracks
-      <ul>
-          {ShowTracks()}
-      </ul>
+      {/* <h1>{category}</h1>
+      TRACKS
+      <ul>{showTracks()}</ul>
       PLAYLIST
-      <ul>
-      {ShowPlaylist()}
-      </ul>
+      <ul>{showPlaylist()}</ul>
       CATEGORIES
-      <ul>
-      {ShowCategories()}
-      </ul>
-      <div className='buttons-row'>
-      <Link to="/attractions">
-        <button>Previous: Attractions</button>
-      </Link>
-      <Link to="/myexperience">
-        <button>Finish Up: My Experience</button>
-      </Link>
+      <ul>{showCategories()}</ul> */}
+      <div className="buttons-row">
+        <Link to="/attractions">
+          <button>Previous: Attractions</button>
+        </Link>
+        <Link to="/myexperience">
+          <button>Finish Up: My Experience</button>
+        </Link>
       </div>
     </div>
   );
